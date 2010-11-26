7f3cc276178af53215969570e066e8ca1873e2f9
yoos <yoos117@gmail.com>
2010-11-26 13:53:22 -0800

    Watchdog is fed at regular intervals.

--- src/joystick.py
+++ src/joystick.py
@@ -11,10 +11,20 @@ import rospy
 from joy.msg import Joy
 from std_msgs.msg import String
 
+rospy.init_node("tric_listener", anonymous=True)
+
+# Comm values
 serialPort = "/dev/ttyUSB0"
 baudRate = 9600
-freq = 50
+freq = 10 # rospy.Rate(10) # 50 Hz
 dogBone = chr(255) # Feed watchdog
+
+time = rospy.get_time()
+
+# Variables
+xAxis = 0
+yAxis = 1
+
 
 # Open serial connection
 try:
@@ -26,22 +36,35 @@ except:
 def feedDog():
     try:
         ser.write(dogBone)
+        rospy.loginfo("Dog fed")
     except:
-        pass
+        rospy.loginfo("Unable to feed dog!")
+
+def watchdogFeeder():
+    global time
+    if rospy.get_time() - time > 1/freq:
+        feedDog()
+        time = rospy.get_time()
+        rospy.loginfo("ROSTime: %s", time)
 
 ############################ ROS get joystick input ###########################
 
 def callback(myJoy):
-    rospy.loginfo("Axis 0: %s   Axis 1: %s", myJoy.axes[0], myJoy.axes[1])
+    xAxisValue = int(255*(myJoy.axes[xAxis]+1)/2) # Range 0-255 in order to send as char value
+    yAxisValue = int(255*(myJoy.axes[yAxis]+1)/2)
+    rospy.loginfo("Axis 0: %s (%s)   Axis 1: %s (%s)", xAxisValue, chr(xAxisValue), yAxisValue, chr(yAxisValue))
+    rospy.loginfo("Axis 0: %s   Axis 1: %s", myJoy.axes[xAxis], myJoy.axes[yAxis])
     try:
-        ser.write(str(myJoy.axes[1]))
+        ser.write(chr(yAxisValue))
     except:
         rospy.loginfo("ERROR: Unable to send data. Check connection.")
 
 def listener():
-    rospy.init_node("listener", anonymous=True)
-    rospy.Subscriber("joy", Joy, callback)
-    rospy.spin()
+    while not rospy.is_shutdown(): # TODO: make feedDog() loop at 50 Hz but not the callback.
+        #rospy.Subscriber("joy", Joy, callback)
+
+        watchdogFeeder()
+        #freq.sleep()
 
 #################################### Qt GUI ###################################
 
