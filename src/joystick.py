5a1e9effc1569aa2804d64e0fa5b5acbf498e16f
yoos <yoos117@gmail.com>
2010-11-26 02:22:57 -0800

    Can send data over serial connection to Arduino.

--- src/joystick.py
+++ src/joystick.py
@@ -22,25 +22,27 @@ def feedDog():
     except:
         pass
 
-# ROS get joystick input
-def callback(myJoy):
-    rospy.loginfo("Axis 0: %s   Axis 1: %s", myJoy.axes[0], myJoy.axes[1])
-    try:
-        ser.write("hello")
-    except:
-        rospy.loginfo("ERROR: Unable to send data. Check connection.")
+########################## ROS get joystick input #############################
 
-def listener():
+def callback(myJoy):
     try:
         ser = serial.Serial(arduinoPort, 9600)
         rospy.loginfo("Arduino at %s", arduinoPort)
     except:
         rospy.loginfo("No Arduino!")
+    rospy.loginfo("Axis 0: %s   Axis 1: %s", myJoy.axes[0], myJoy.axes[1])
+    try:
+        ser.write(str(myJoy.axes[1]))
+    except:
+        rospy.loginfo("ERROR: Unable to send data. Check connection.")
+
+def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("joy", Joy, callback)
     rospy.spin()
 
-# Qt GUI
+#################################### Qt GUI ###################################
+
 class MainWindow(Qt.QMainWindow):
     def __init__(self, *args):
         apply(Qt.QMainWindow.__init__, (self,) + args)
@@ -54,7 +56,8 @@ class CtrlButton(Qt.QPushButton):
 
 #mainWin = MainWindow()
 
-# Main
+#################################### Main #####################################
+
 if __name__ == "__main__" :
     listener()
     #mainWin.show()
