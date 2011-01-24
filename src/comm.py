0226df10cd6351569e4d03f37c668371c91efa1e
yoos <yoos117@gmail.com>
2011-01-23 22:25:55 -0800

    Having a separate Publisher and Subscriber system turned out to be pointless. Merged again.

--- src/comm.py
+++ src/comm.py
@@ -19,6 +19,28 @@ serHeader = chr(255)
 okayToSend = False
 verboseOn = True
 
+armed = False
+
+# Variables
+xAxis = 0   # X axis
+yAxis = 1   # Y axis
+tAxis = 2   # Twist axis
+zAxis = 3   # Z axis
+hAxis = 4   # Top horizontal
+vAxis = 5   # Top vertical
+
+# Axis sign flips
+xSign = -1
+ySign = 1 
+tSign = -1
+zSign = 1 
+hSign = -1
+vSign = 1 
+
+# Axis values
+axisValues = [126, 126, 126, 126]   # Keep Z value at non-zero so user is forced to fiddle with throttle before motors arm. Hopefully prevents disasters.
+buttonValues = []
+
 rospy.init_node("tric_comm_node", anonymous=True)
 
 # Open serial connection
@@ -31,18 +53,34 @@ except:
 
 ############################ ROS get joystick input ###########################
 
-def callback(joyPub):
-    try:
-        ser.write(serHeader + chr(joyPub.axisInputs[0]) + chr(joyPub.axisInputs[1]) + chr(joyPub.axisInputs[2]) + chr(joyPub.axisInputs[3]))
-        if verboseOn: rospy.logerr("A0: %s   A1: %s   A2: %s   A3: %s", joyPub.axisInputs[0], joyPub.axisInputs[1], joyPub.axisInputs[2], joyPub.axisInputs[3])
-    except:
-        if verboseOn: rospy.logerr("ERROR: Unable to send data. Check connection.")
+def callback(myJoy):
+    global axisValues, armed
+    # Calculate axis values.
+    axisValues[0] = int(250*(xSign * myJoy.axes[xAxis] + 1) / 2 + 1)   # Range 1-251 in order to send as char value
+    axisValues[1] = int(250*(ySign * myJoy.axes[yAxis] + 1) / 2 + 1)
+    axisValues[2] = int(250*(tSign * myJoy.axes[tAxis] + 1) / 2 + 1)
+    axisValues[3] = int(250*(zSign * myJoy.axes[zAxis] + 1) / 2 + 1)
+
+    if armed:
+        sendData()
+    elif not armed:
+        rospy.logerr("Move joystick throttle to minimum position in order to send motor arming signal.")
+        if axisValues[3] == 1:   # If throttle is at minimum position
+            armed = True
+            rospy.logerr("Joystick throttle at minimum! Motors armed!")
+    
     rospy.sleep(dataSendInterval)
 
+def sendData():
+    try:
+        ser.write(serHeader + chr(axisValues[0]) + chr(axisValues[1]) + chr(axisValues[2]) + chr(axisValues[3]))
+        if verboseOn: rospy.logerr("A0: %s   A1: %s   A2: %s   A3: %s", axisValues[0], axisValues[1], axisValues[2], axisValues[3])
+    except:
+        if verboseOn: rospy.logerr("ERROR: Unable to send data. Check connection.")
 
-def listener():
+def tric_comm():
     while not rospy.is_shutdown():
-        rospy.Subscriber("tric_joy_publisher", TricJoy, callback, queue_size=1)
+        rospy.Subscriber("joy", Joy, callback, queue_size=1)
         rospy.spin()
 
 #################################### Qt GUI ###################################
@@ -63,6 +101,9 @@ class CtrlButton(Qt.QPushButton):
 #################################### Main #####################################
 
 if __name__ == "__main__" :
-    listener()
+    try:
+        tric_comm()
+    except rospy.ROSInterruptException:
+        pass
     #mainWin.show()
 
