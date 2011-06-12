f25d77e2c830890aea5d8ee175ef813421964b08
yoos <yoos117@gmail.com>
2011-06-11 20:48:04 -0700

    Lots of debugging. Don't know what I changed. Head hurts.

--- src/comm.py
+++ src/comm.py
@@ -26,7 +26,7 @@ verboseOn = True
 armed = False
 
 axisSigns = [-1, 1, -1, 1, -1, 1]   # Axis sign flips
-axisValues = [126, 126, 126, 18]   # Keep Z value at some non-zero value (albeit very low so the tricopter doesn't fly off if something goes awry) so user is forced to fiddle with throttle before motors arm. Hopefully prevents disasters.
+axisValues = [126, 126, 126, 2]   # Keep Z value at some non-zero value (albeit very low so the tricopter doesn't fly off if something goes awry) so user is forced to fiddle with throttle before motors arm. Hopefully prevents disasters.
 buttonValues = []
 
 rospy.init_node("tric_comm_node", anonymous=True)
@@ -34,7 +34,7 @@ rospy.init_node("tric_comm_node", anonymous=True)
 # Open serial connection
 try:
     ser = serial.Serial(serialPort, baudRate)
-    rospy.logerr("Arduino at %s", serialPort)
+    rospy.loginfo("Arduino at %s", serialPort)
 except:
     rospy.logerr("No Arduino at %s!", serialPort)
 
@@ -42,29 +42,31 @@ except:
 ############################ ROS get joystick input ###########################
 
 def callback(myJoy):
-    global axisValues, armed
+    global axisValues
     # Calculate axis values.
     axisValues[0] = int(250*(axisSigns[0] * myJoy.axes[0] + 1) / 2 + 1)   # Range 1-251 in order to send as char value
     axisValues[1] = int(250*(axisSigns[1] * myJoy.axes[1] + 1) / 2 + 1)
     axisValues[2] = int(250*(axisSigns[2] * myJoy.axes[2] + 1) / 2 + 1)
     axisValues[3] = int(250*(axisSigns[3] * myJoy.axes[3] + 1) / 2 + 1)
+    rospy.loginfo("Joystick moved!")
 
+def communicate():
+    global armed
     if armed:
         sendData(serHeader + chr(axisValues[0]) + chr(axisValues[1]) + chr(axisValues[2]) + chr(axisValues[3]))
-        if verboseOn: rospy.logerr("A0: %s   A1: %s   A2: %s   A3: %s", axisValues[0], axisValues[1], axisValues[2], axisValues[3])
+        if verboseOn: rospy.loginfo("A0: %s   A1: %s   A2: %s   A3: %s", axisValues[0], axisValues[1], axisValues[2], axisValues[3])
     elif not armed:
-        rospy.logerr("Move joystick throttle to minimum position in order to send motor arming signal.")
+        rospy.loginfo("Move joystick throttle to minimum position in order to send motor arming signal.")
         if axisValues[3] == 1:   # If throttle is at minimum position
             armed = True
-            rospy.logerr("Joystick throttle at minimum! Motors armed!")
+            rospy.loginfo("Joystick throttle at minimum! Motors armed!")
     
-    rospy.sleep(dataSendInterval)
-
 def sendData(myStr):
     try:
         ser.write(myStr)
     except:
         if verboseOn: rospy.logerr("ERROR: Unable to send data. Check connection.")
+
 
 class TricSubscriber(threading.Thread):
     def __init__(self):
@@ -75,6 +77,18 @@ class TricSubscriber(threading.Thread):
             rospy.Subscriber("joy", Joy, callback, queue_size=1)
             rospy.spin()
 
+class TricCommunicator(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.times = 0
+    def run(self):
+        while self.running and not rospy.is_shutdown():
+            self.times += 1
+            communicate()
+            rospy.loginfo(self.times)
+            rospy.sleep(dataSendInterval)
+
 class TricWatchdog(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -84,7 +98,7 @@ class TricWatchdog(threading.Thread):
         while self.running and not rospy.is_shutdown():
             self.times += 1
             sendData(dogBone)
-            rospy.loginfo(self.times)
+            # rospy.loginfo(self.times)
             rospy.sleep(dogFeedInterval)
             
 
@@ -99,7 +113,7 @@ class TricWatchdog(threading.Thread):
 #        apply(Qt.QPushButton.__init__, (self,) + args)
 #        self.connect(self, SIGNAL("clicked()"), self.doPrint)
 #    def doPrint(self):
-#        rospy.logerr("Control button clicked")
+#        rospy.loginfo("Control button clicked")
 
 #mainWin = MainWindow()
 
@@ -109,16 +123,20 @@ if __name__ == "__main__" :
     try:
         tricSub = TricSubscriber()
         tricSub.start()   # Start subscriber thread.
+        tricComm = TricCommunicator()
+        tricComm.start()
         tricWatch = TricWatchdog()
         tricWatch.start()   # Start watchdog thread.
         raw_input("Hit <enter> to quit")
 
         # Stop the while loops
         tricSub.running = False
+        tricComm.running = False
         tricWatch.running = False
 
         # Wait for threads to finish jobs
         tricSub.join()
+        tricComm.join()
         tricWatch.join()
 
     except rospy.ROSInterruptException:
--- src/flightcode/system.cpp
+++ src/flightcode/system.cpp
@@ -20,8 +20,11 @@ void System::Run() {
         // Serial.println("System: Motors not armed.");
         for (int i=0; i<3; i++) {
             motor[i].write(0);   // Send some value that is not minimum throttle.
+            Serial.print(motorVal[i]);
+            Serial.print("   ");
         }
         tailServo.write(90);
+        Serial.println(tailServoVal);
         if (motorVal[MT] == TMIN && motorVal[MR] == TMIN && motorVal[ML] == TMIN) {
             armed = true;
             Serial.println("System: Motors armed.");
@@ -30,8 +33,11 @@ void System::Run() {
     else {
         for (int i=0; i<3; i++) {
             motor[i].write(motorVal[i]);   // Write motor values to motors.
+            Serial.print(motorVal[i]);
+            Serial.print("   ");
         }
         tailServo.write(tailServoVal);
+        Serial.println(tailServoVal);
         #ifdef DEBUG
         Serial.println("System wrote to motors.");
         #endif
@@ -39,13 +45,21 @@ void System::Run() {
 }
 
 void System::Die() {
-    for (int i=0; i<3; i++) {
-        motorVal[i] = TMIN;   // This is inelegant. Is there a way to avoid running both of these?
-        motor[i].write(TMIN);
+    if (!armed) {
+        for (int i=0; i<3; i++) {
+            motor[i].write(TMIN);   // Send some value that is not minimum throttle.
+        }
     }
-    #ifdef DEBUG
-    Serial.println("System dead!");
-    #endif
+    else {
+        for (int i=0; i<3; i++) {
+            motorVal[i] = TMIN;   // This is inelegant. Is there a way to avoid running both of these?
+            motor[i].write(TMIN);
+        }
+        tailServo.write(90);
+        #ifdef DEBUG
+        Serial.println("System dead!");
+        #endif
+    }
 }
 
 void System::SetMotor(int mNum, int mVal) {
