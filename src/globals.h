3e6fe7944c3c03118882bf24b15986fca3248a18
yoos <yoos117@gmail.com>
2011-12-02 19:50:59 -0800

    Better counterbalance found.

--- src/globals.h
+++ src/globals.h
@@ -97,7 +97,7 @@ struct PIDdata {
 #define MOTOR_T_OFFSET 0   // Speed offset for tail motor.
 #define MOTOR_R_OFFSET 0   // Speed offset for right motor.
 #define MOTOR_L_OFFSET 0   // Speed offset for left motor.
-#define MOTOR_T_SCALE  1.03   // Scale speed of tail motor.
+#define MOTOR_T_SCALE  1.02   // Scale speed of tail motor.
 #define MOTOR_R_SCALE  1   // Scale speed of right motor.
 #define MOTOR_L_SCALE  1   // Scale speed of left motor.
 #define TAIL_SERVO_DEFAULT_POSITION 69
