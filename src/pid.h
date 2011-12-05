4755388e2d5c0493004c34fb26d86259e27b3fa3
yoos <yoos117@gmail.com>
2011-12-04 19:23:51 -0800

    Zero integral term once overshoot is detected.

--- src/pid.h
+++ src/pid.h
@@ -13,9 +13,16 @@ float updatePID(float targetValue, float currentValue, struct PIDdata &PIDparame
     PIDparameters.integral += proportional * deltaPIDTime;
 
     derivative = (currentValue - PIDparameters.lastValue) / deltaPIDTime;
-
     PIDparameters.lastValue = currentValue;
 
+    // Zero integral once overshoot is detected.
+    if ((proportional < 0 && derivative > 0) ||
+        (proportional > 0 && derivative < 0)) {
+        PIDparameters.integral = 0;
+    }
+
+    // NOTE: The P, I, and D gains should end up being no more than a single
+    // degree of magnitude away from 1.
     return PIDparameters.P * proportional +
            PIDparameters.I * PIDparameters.integral +
            PIDparameters.D * derivative;
