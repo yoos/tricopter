2c9cec80013cf776d8402989355858a1f628dd64
yoos <yoos117@gmail.com>
2012-02-11 01:34:06 -0800

    Include configuration for serialmon.py.

--- src/triconfig.py
+++ src/triconfig.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# =============================================================================
 # Communication
+# =============================================================================
+
+# General
 #serialPort = "/dev/ttyUSB0"   # Uncomment to specify serial port. Otherwise, will connect to first available port.
 baudRate = 57600
+
+# TX
 dataSendInterval = 0.030   # 30 ms interval = 33.3 hz. NOTE: This frequency should be LOWER than the microcontroller's control loop frequency!
 dogFeedInterval = 0.1
 serHeader = chr(255)
 dogBone = chr(254)
+
+# RX
+newlineSerTag  = '\xde\xad\xbe\xef'
+fieldSerTag    = '\xff\xff'
+dcmSerTag      = '\xfb'
+rotationSerTag = '\xfc'
+motorSerTag    = '\xfd'
+pidSerTag      = '\xfe'
+
 
 # Joystick axis sign flips.
 axisSigns = [-1, 1, 1, 1, -1, 1]
