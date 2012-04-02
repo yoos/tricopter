768002cd8adfdb96ab258d953bda8f6aa92bc868
Soo-Hyun Yoo <yoos117@gmail.com>
2012-04-01 21:46:30 -0700

    Working better.

--- src/lsm303.cpp
+++ src/lsm303.cpp
@@ -1,25 +1,38 @@
-/*! \file lsm303dlm.cpp
+/*! \file lsm303.cpp
  *  \author Soo-Hyun Yoo
- *  \brief Source for LSM303DLM magnetometer/accelerometer class.
+ *  \brief Source for LSM303 magnetometer/accelerometer class.
  *
  *  Details.
  */
 
-#include "lsm303dlm.h"
+#include "lsm303.h"
 
-LSM303DLM::LSM303DLM() {
-    // Put into continuous measurement mode (DS p. 12).
-    sendI2C(MAG_ADDRESS, 0x02, 0x00);
+LSM303::LSM303() {
+    readI2C(MAG_ADDRESS, 0x0f, 1, buffer);
+    sp("LSM303 ID: ");
+    spln((int) buffer[0]);
+
+    // Set magnetometer data rate to 75 Hz (DS p. 33).
+    readI2C(MAG_ADDRESS, 0x00, 1, buffer);
+    buffer[0] &= ~(7<<2);   // Clear out relevant register.
+    buffer[0] |= 6<<2;
+    sendI2C(MAG_ADDRESS, 0x00, buffer[0]);
+
+    // Set magnetometer operation mode to continuous-conversion (DS p. 34).
+    readI2C(MAG_ADDRESS, 0x02, 1, buffer);
+    buffer[0] &= ~(3);   // Clear out relevant register.
+    buffer[0] |= 0;
+    sendI2C(MAG_ADDRESS, 0x02, buffer[0]);
 }
 
-void LSM303DLM::poll() {
+void LSM303::poll() {
     // Read data.
     readI2C(MAG_ADDRESS, 0x03, 6, buffer);
 
     // Read in high and low bytes.
     mRaw[0] = ((buffer[0] << 8) | buffer[1]);   // X
-    mRaw[1] = ((buffer[2] << 8) | buffer[3]);   // Y
-    mRaw[2] = ((buffer[4] << 8) | buffer[5]);   // Z
+    mRaw[1] = ((buffer[4] << 8) | buffer[5]);   // Y
+    mRaw[2] = ((buffer[2] << 8) | buffer[3]);   // Z
 
     // Convert raw outputs to vector values.
     // Output: [0xf800 -- 0xffff] = [-2048 --   -1]
@@ -32,16 +45,16 @@ void LSM303DLM::poll() {
         else
             tmp = mRaw[i];
 
-        mVec[i] = tmp;
+        mVec[i] = (int16_t) mRaw[i];
     }
-    vNorm(mVec);   // Normalize.
+    //vNorm(mVec);   // Normalize.
 }
 
-float* LSM303DLM::get() {
+float* LSM303::get() {
     return mVec;
 }
 
-float LSM303DLM::get(int axis) {
+float LSM303::get(int axis) {
     return mVec[axis];
 }
 
--- src/lsm303.h
+++ src/lsm303.h
@@ -1,30 +1,31 @@
-/*! \file lsm303dlm.h
+/*! \file lsm303.h
  *  \author Soo-Hyun Yoo
- *  \brief Header for LSM303DLM magnetometer/accelerometer class.
+ *  \brief Header for LSM303 magnetometer/accelerometer class.
  *
  *  Details.
  */
 
-#ifndef LSM303DLM_H
-#define LSM303DLM_H
+#ifndef LSM303_H
+#define LSM303_H
 
 #include "i2c.h"
 #include "triMath.h"
 #include "globals.h"
 
 #define MAG_ADDRESS 0x1e
+#define ACC_ADDRESS 0x18
 
-class LSM303DLM {
+class LSM303 {
     uint8_t buffer[6];
     uint16_t mRaw[3];
     float mVec[3];
 
 public:
-    LSM303DLM();
+    LSM303();
     void poll();   // Get bits from ITG-3200 and update gVal[].
     float* get();
     float get(int);
 };
 
-#endif // LSM303DLM_H
+#endif // LSM303_H
 
