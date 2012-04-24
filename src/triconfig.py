5be8e5689b667e94ebb3f4c32ec6b75719ed5ce8
Soo-Hyun Yoo <yoos117@gmail.com>
2012-04-24 01:29:27 -0700

    Small changes.

--- src/triconfig.py
+++ src/triconfig.py
@@ -9,14 +9,15 @@
 #serialPort = "/dev/ttyUSB0"   # Uncomment to specify serial port. Otherwise, will connect to first available port.
 baudRate = 57600
 debug = False
+loopPeriod = 0.005   # 5 ms interval = 200 Hz.
 
 # TX
-dataSendInterval = 0.025   # 25 ms interval = 40 Hz. NOTE: This frequency should be LOWER than the microcontroller's control loop frequency!
-dogFeedInterval = 0.1
-serHeader = chr(255)
-dogBone = chr(254)
+txInterval = 5   # 200 Hz / 5 = 40 Hz.
+serHeader = '\xff'
+#dogBone = '\xfe'
 
 # RX
+rxInterval = 1   # 200 Hz / 5 = 200 Hz.
 newlineSerTag  = '\xde\xad\xbe\xef'
 fieldSerTag    = '\xff\xff'
 dcmSerTag      = '\xfb'
