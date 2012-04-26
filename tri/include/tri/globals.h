19fa69cf36632ad0f6a0761bbe48923a801cb3e1
Soo-Hyun Yoo <yoos117@gmail.com>
2012-04-26 11:14:15 -0700

    Some changes.

--- tri/include/tri/globals.h
+++ tri/include/tri/globals.h
@@ -8,6 +8,8 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <WProgram.h>
+
 // #define DEBUG
 
 // ============================================================================
--- tri_flight_control/include/tri_flight_control/watchdog.h
+++ tri_flight_control/include/tri_flight_control/watchdog.h
@@ -8,6 +8,8 @@
 #ifndef WATCHDOG_H
 #define WATCHDOG_H
 
+#include <tri/globals.h>
+
 class Watchdog {
     long int time;   // Make sure this doesn't overflow!
     int deadCycle;
