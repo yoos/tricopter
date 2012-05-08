e2af468a65b7bc56859cc55c0a005678e9fc72f6
Soo-Hyun Yoo <yoos117@gmail.com>
2012-05-07 23:07:16 -0700

    Remove ambiguity by renaming pwmOut to pwmOutput.

--- src/motors.h
+++ src/motors.h
@@ -34,7 +34,7 @@ void angular_rate_controller (float* desired_rate, float* current_rate, int16_t*
     }
 }
 
-void calculate_pwm_outputs(float pwmThrottle, int16_t* pwmShift, int16_t* pwmOut) {
+void calculate_pwm_outputs(float pwmThrottle, int16_t* pwmShift, int16_t* pwmOutput) {
     // ====================================================================
     // Calculate motor/servo values.
     //     MOTOR_X_OFFSET: Offset starting motor values to account for
@@ -45,17 +45,17 @@ void calculate_pwm_outputs(float pwmThrottle, int16_t* pwmShift, int16_t* pwmOut
     // TODO: MOTOR_X_SCALE should be replaced by actual PID gains. Besides,
     // it's incorrect to scale in the negative direction if the
     // corresponding arm is heavier.
-    // TODO: The last term for each pwmOut is INACCURATE. Fix this.
+    // TODO: The last term for each pwmOutput is INACCURATE. Fix this.
     // ====================================================================
-    pwmOut[SERVO_T] = SERVO_US_NEUTRAL + pwmShift[2];
+    pwmOutput[SERVO_T] = SERVO_US_NEUTRAL + pwmShift[2];
 
-    pwmOut[MOTOR_T] = (pwmThrottle + -pwmShift[0]) / cos(((float) pwmOut[SERVO_T]-SERVO_US_ZERO)/SERVO_US_PER_RAD);
-    pwmOut[MOTOR_R] =  pwmThrottle +  pwmShift[0] - pwmShift[1]*sqrt(3);
-    pwmOut[MOTOR_L] =  pwmThrottle +  pwmShift[0] + pwmShift[1]*sqrt(3);
+    pwmOutput[MOTOR_T] = (pwmThrottle + -pwmShift[0]) / cos(((float) pwmOutput[SERVO_T]-SERVO_US_ZERO)/SERVO_US_PER_RAD);
+    pwmOutput[MOTOR_R] =  pwmThrottle +  pwmShift[0] - pwmShift[1]*sqrt(3);
+    pwmOutput[MOTOR_L] =  pwmThrottle +  pwmShift[0] + pwmShift[1]*sqrt(3);
 
-    pwmOut[MOTOR_T] = TMIN + MOTOR_T_OFFSET + MOTOR_T_SCALE * pwmOut[MOTOR_T];
-    pwmOut[MOTOR_R] = TMIN + MOTOR_R_OFFSET + MOTOR_R_SCALE * pwmOut[MOTOR_R];
-    pwmOut[MOTOR_L] = TMIN + MOTOR_L_OFFSET + MOTOR_L_SCALE * pwmOut[MOTOR_L];
+    pwmOutput[MOTOR_T] = TMIN + MOTOR_T_OFFSET + MOTOR_T_SCALE * pwmOutput[MOTOR_T];
+    pwmOutput[MOTOR_R] = TMIN + MOTOR_R_OFFSET + MOTOR_R_SCALE * pwmOutput[MOTOR_R];
+    pwmOutput[MOTOR_L] = TMIN + MOTOR_L_OFFSET + MOTOR_L_SCALE * pwmOutput[MOTOR_L];
 
     // ====================================================================
     // After finding the maximum and minimum motor values, limit, but NOT
@@ -63,16 +63,16 @@ void calculate_pwm_outputs(float pwmThrottle, int16_t* pwmShift, int16_t* pwmOut
     // Doing this incorrectly will result in motor values seemingly stuck
     // mostly at either extremes.
     // ====================================================================
-    int mapUpper = pwmOut[MOTOR_T] > pwmOut[MOTOR_R] ? pwmOut[MOTOR_T] : pwmOut[MOTOR_R];
-    mapUpper = mapUpper > pwmOut[MOTOR_L] ? mapUpper : pwmOut[MOTOR_L];
+    int mapUpper = pwmOutput[MOTOR_T] > pwmOutput[MOTOR_R] ? pwmOutput[MOTOR_T] : pwmOutput[MOTOR_R];
+    mapUpper = mapUpper > pwmOutput[MOTOR_L] ? mapUpper : pwmOutput[MOTOR_L];
     mapUpper = mapUpper > TMAX ? mapUpper : TMAX;
 
-    int mapLower = pwmOut[MOTOR_T] < pwmOut[MOTOR_R] ? pwmOut[MOTOR_T] : pwmOut[MOTOR_R];
-    mapLower = mapLower < pwmOut[MOTOR_L] ? mapLower : pwmOut[MOTOR_L];
+    int mapLower = pwmOutput[MOTOR_T] < pwmOutput[MOTOR_R] ? pwmOutput[MOTOR_T] : pwmOutput[MOTOR_R];
+    mapLower = mapLower < pwmOutput[MOTOR_L] ? mapLower : pwmOutput[MOTOR_L];
     mapLower = mapLower < TMIN ? mapLower : TMIN;
 
     // We shouldn't have to use these, but uncomment the following two
-    // lines if pwmOut goes crazy and makes mapUpper lower than mapLower:
+    // lines if pwmOutput goes crazy and makes mapUpper lower than mapLower:
     //mapUpper = mapUpper > TMIN ? mapUpper : TMIN+1;
     //mapLower = mapLower < TMAX ? mapLower : TMAX-1;
 
@@ -83,10 +83,10 @@ void calculate_pwm_outputs(float pwmThrottle, int16_t* pwmShift, int16_t* pwmOut
     // ====================================================================
     for (int i=0; i<3; i++) {
         if (mapUpper > mapLower) {
-            pwmOut[i] = map(pwmOut[i], mapLower, mapUpper, TMIN, TMAX);
+            pwmOutput[i] = map(pwmOutput[i], mapLower, mapUpper, TMIN, TMAX);
         }
         else {
-            pwmOut[i] = TMIN;
+            pwmOutput[i] = TMIN;
         }
     }
 }
